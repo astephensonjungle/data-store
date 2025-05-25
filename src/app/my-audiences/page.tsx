88dc073967b0b648c8dfcd81d0cf2c80c0c3d90b
astephensonjungle <ashley.stephenson@jungledesign.co>
2025-05-25 14:35:42 +0100

    remove buttons

--- src/app/my-audiences/page.tsx
+++ src/app/my-audiences/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
 import AudienceCard from "@/components/AudienceCard";
 import AudienceSheet from "@/components/AudienceSheet";
 import { useAudiences } from "@/hooks/useAudiences";
