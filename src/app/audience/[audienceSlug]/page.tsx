b45c700473f59a24422209ad4a08bb40526c53f4
Filip Kantedal <kantedal@gmail.com>
2025-06-27 13:16:46 +0200

    fix scroll bug

--- src/app/audience/[audienceSlug]/page.tsx
+++ src/app/audience/[audienceSlug]/page.tsx
@@ -15,46 +15,48 @@ export default async function AudiencePage({ params }: { params: Promise<{ audie
 
 	return (
 		<HydrateClient>
-			<main className="mx-auto flex w-full max-w-7xl flex-col gap-12 p-8">
-				<div className="flex flex-row gap-20">
-					<div className="flex flex-1 flex-col gap-6">
-						<div className="flex flex-row gap-4">
-							<Image
-								src={`/assets/brand-images/${audience.slug}.webp`}
-								alt={audience.name}
-								width={200}
-								height={200}
-								className="h-[200px] w-[200px] flex-none overflow-hidden rounded-2xl"
-							/>
-							<div className="flex flex-col gap-2">
-								<h2 className="font-medium text-xl">{audience.name}</h2>
-								<p className="text-base text-muted-foreground">
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius cupiditate, minima sequi dolore
-									perferendis nobis odit omnis doloribus at, voluptas a deserunt nulla accusamus illum architecto quod
-									nam deleniti magni.
-								</p>
+			<div className="h-[calc(100vh-64px)] overflow-y-auto p-8">
+				<main className="mx-auto flex w-full max-w-7xl flex-col gap-12">
+					<div className="flex flex-row gap-20">
+						<div className="flex flex-1 flex-col gap-6">
+							<div className="flex flex-row gap-4">
+								<Image
+									src={`/assets/brand-images/${audience.slug}.webp`}
+									alt={audience.name}
+									width={200}
+									height={200}
+									className="h-[200px] w-[200px] flex-none overflow-hidden rounded-2xl"
+								/>
+								<div className="flex flex-col gap-2">
+									<h2 className="font-medium text-xl">{audience.name}</h2>
+									<p className="text-base text-muted-foreground">
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius cupiditate, minima sequi dolore
+										perferendis nobis odit omnis doloribus at, voluptas a deserunt nulla accusamus illum architecto quod
+										nam deleniti magni.
+									</p>
+								</div>
 							</div>
+							<AudienceAccordion />
 						</div>
-						<AudienceAccordion />
+
+						<ConsumersInformationCard audience={audience} />
 					</div>
 
-					<ConsumersInformationCard audience={audience} />
-				</div>
-
-				<div>
-					<h2 className="mb-3 font-medium text-2xl">Explore sample products from this brand</h2>
-					<RelatedProductsList />
-				</div>
-
-				<div>
-					<h2 className="mb-3 font-medium text-2xl">Explore more audiences</h2>
-					<div className="flex w-full gap-2 overflow-x-auto">
-						{relatedAudiences.map((audience) => (
-							<AudienceCard audience={audience} key={audience.slug} />
-						))}
+					<div>
+						<h2 className="mb-3 font-medium text-2xl">Explore sample products from this brand</h2>
+						<RelatedProductsList />
 					</div>
-				</div>
-			</main>
+
+					<div>
+						<h2 className="mb-3 font-medium text-2xl">Explore more audiences</h2>
+						<div className="flex w-full gap-2 overflow-x-auto">
+							{relatedAudiences.map((audience) => (
+								<AudienceCard audience={audience} key={audience.slug} />
+							))}
+						</div>
+					</div>
+				</main>
+			</div>
 		</HydrateClient>
 	);
 }
