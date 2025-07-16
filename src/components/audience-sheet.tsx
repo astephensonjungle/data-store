ee7b58a4308e48a30dc383633860f1a767963376
Filip Kantedal <kantedal@gmail.com>
2025-07-16 14:17:40 +0200

    audience sheet fix

--- src/components/audience-sheet.tsx
+++ src/components/audience-sheet.tsx
@@ -50,10 +50,12 @@ export default function AudienceSheet({
 		<Sheet open={open && !!savedAudienceId} onOpenChange={onOpenChange}>
 			<SheetContent className="w-full max-w-xl text-sm">
 				<SheetHeader>
-					<SheetTitle>{isLoading ? <Skeleton className="h-6 w-48" /> : audience?.name}</SheetTitle>
+					<SheetTitle className="font-medium text-xl">
+						{isLoading ? <Skeleton className="h-6 w-48" /> : audience?.name}
+					</SheetTitle>
 				</SheetHeader>
 				<div className="flex h-full flex-1 flex-col gap-4 p-6 pt-0">
-					<div className="flex-1">
+					<div className="flex flex-1 flex-col gap-8">
 						<div className="flex flex-col divide-y">
 							<div className="flex justify-between py-3">
 								<div className="font-medium">Brand</div>
@@ -76,62 +78,70 @@ export default function AudienceSheet({
 								</div>
 							</div>
 						</div>
-					</div>
 
-					<div className="rounded-lg bg-[#EEEAE2] p-4">
-						<h3 className="font-medium text-lg">Audience Details</h3>
-						<p className="text-muted-foreground text-sm">
-							Understand who is in the audience based on real shopper behavior.
-						</p>
-						<div className="mt-2 flex flex-col divide-y">
-							<div className="flex justify-between py-2">
-								<div className="font-medium">Audience Count</div>
-								<div className="text-foreground">
-									{isLoading ? <Skeleton className="h-5 w-20" /> : new Intl.NumberFormat("en-US").format(audienceCount)}
+						<div className="rounded-lg bg-[#EEEAE2] p-4">
+							<h3 className="font-medium text-lg">Audience Details</h3>
+							<p className="mt-1.5 text-muted-foreground text-sm">
+								Understand who is in the audience based on real shopper behavior.
+							</p>
+							<div className="mt-3 flex flex-col divide-y">
+								<div className="flex justify-between py-2">
+									<div className="font-medium">Audience Count</div>
+									<div className="text-foreground">
+										{isLoading ? (
+											<Skeleton className="h-5 w-20" />
+										) : (
+											new Intl.NumberFormat("en-US").format(audienceCount)
+										)}
+									</div>
+								</div>
+								<div className="flex justify-between py-2">
+									<div className="font-medium">Audience Type</div>
+									<div className="text-foreground">
+										{isLoading ? (
+											<Skeleton className="h-5 w-28" />
+										) : savedAudience?.audienceType === AudienceType.DETERMINISTIC ? (
+											"Deterministic"
+										) : (
+											"Extended"
+										)}
+									</div>
+								</div>
+								<div className="flex justify-between py-2">
+									<div className="font-medium">Lookback window</div>{" "}
+									<div className="text-foreground">
+										{isLoading ? (
+											<Skeleton className="h-5 w-24" />
+										) : savedAudience?.lookbackWindow === LookbackWindow.DAYS_90 ? (
+											"90 days"
+										) : (
+											"1 year"
+										)}
+									</div>
 								</div>
 							</div>
-							<div className="flex justify-between py-2">
-								<div className="font-medium">Audience Type</div>
-								<div className="text-foreground">
-									{isLoading ? (
-										<Skeleton className="h-5 w-28" />
-									) : savedAudience?.audienceType === AudienceType.DETERMINISTIC ? (
-										"Deterministic"
-									) : (
-										"Extended"
-									)}
-								</div>
-							</div>
-							<div className="flex justify-between py-2">
-								<div className="font-medium">Lookback window</div>{" "}
-								<div className="text-foreground">
-									{isLoading ? (
-										<Skeleton className="h-5 w-24" />
-									) : savedAudience?.lookbackWindow === LookbackWindow.DAYS_90 ? (
-										"90 days"
-									) : (
-										"1 year"
-									)}
+						</div>
+
+						<div className="rounded-lg bg-[#EEEAE2] p-4">
+							<h3 className="font-medium text-lg">Activation Details</h3>
+							<p className="mt-1.5 text-muted-foreground text-sm">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+							</p>
+							<div className="mt-3 flex flex-col divide-y">
+								<div className="flex justify-between py-2">
+									<div className="font-medium">Status</div>
+									<div className="text-foreground">Unknown</div>
 								</div>
 							</div>
 						</div>
 					</div>
 
-					<div className="rounded-lg bg-[#EEEAE2] p-4">
-						<h3 className="font-medium text-lg">Activation Details</h3>
-						<p className="text-muted-foreground text-sm">
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-						</p>
-						<div className="mt-2 flex flex-col divide-y">
-							<div className="flex justify-between py-2">
-								<div className="font-medium">Status</div>
-								<div className="text-foreground">Unknown</div>
-							</div>
+					<div className="flex items-center justify-between gap-20 pb-6">
+						<div className="flex flex-col gap-1.5">
+							<h3 className="font-medium text-base">Activate Audience</h3>
+							<p className="text-muted-foreground text-sm">Via LiveRamp Connect and a Trade Desk Seat ID</p>
 						</div>
-					</div>
-
-					<div className="flex justify-end">
-						<Button>Activate Audience</Button>
+						<Button size="lg">Activate Audience</Button>
 					</div>
 				</div>
 			</SheetContent>
